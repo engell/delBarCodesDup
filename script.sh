898eefb55019f3c4f87316168c978030b747fe72
engell <dark.engell.25@gmail.com>
2017-08-22 16:27:16 -0700

    Extrae dup

--- script.sh
+++ script.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+rm tmp/mar2
 rm tmp/mar
+
 awk -f "./extraeMar.awk" | sort | uniq -D -w 14 | cut -d " " -f 2 >> tmp/mar
 
-
+for linea in $(man tmp/mar);
+    do
+        head -$linea tmp/CatalogoMar.txt | tail -1 >> tmp/mar2
+    done
