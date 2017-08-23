be14132e5a7b79d2aaef864782031d95c656ef37
engell <dark.engell.25@gmail.com>
2017-08-23 01:58:38 -0700

    good sort

--- script.sh
+++ script.sh
@@ -3,11 +3,11 @@ rm tmp/mar2
 rm tmp/mar
 
 sed -i '4s/mar2/CatalogoMar.txt/gi' './extrae.awk'
-awk -f "./extrae.awk" | sort | uniq -D -w 14 | cut -d " " -f 2 >> tmp/mar
+awk -f "./extrae.awk" | sed 's/ //g' | sort | uniq -D -w 14 | cut -d "." -f 2 >> tmp/mar
 
 for linea in $(man tmp/mar);
     do
         head -$linea tmp/CatalogoMar.txt | tail -1 >> tmp/mar2
     done
 sed -i '4s/CatalogoMar.txt/mar2/gi' './extrae.awk'
-awk -f "./extrae.awk" 
+awk -f "./extrae.awk" | sed 's/ //g' | cut -d "." -f 1,3 | sort -g -r
