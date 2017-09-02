e9bdbbbeae4e5a4a3f683b05297210df423cac25
engell <dark.engell.25@gmail.com>
2017-09-02 13:20:51 -0700

    Extrae lineas duplicadas a un archivo llamado less

--- script.sh
+++ script.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+rm tmp/cat
 
-cat=$(awk -f "./extrae.awk" | sed 's/ /0/g' | sort | uniq -d -w 14 | cut -d "." -f 3 | sort -g -r) #>> tmp/cat
+awk -f "./extrae.awk" | sed 's/ /0/g' | sort | uniq -d -w 14 | cut -d "." -f 3 | sort -g -r >> tmp/cat
 sed -i 's/$/d/' 'tmp/cat'
 
-for linea in $cat;
-    do
-        sed -i $linea tmp/Catalogo.txt
-    done
+for linea in $(man tmp/cat); do
+    num=$(echo $linea | sed 's/.$//g')
+    awk "NR==$num" tmp/Catalogo.txt >> tmp/less
+    sed -i $linea tmp/Catalogo.txt
+done
